17514b2f0c6c9fab08014443c549e7fb6b4e97b0
aetxabao <aitor.etxabarren@mariaanasanz.es>
2020-12-12 12:42:45 +0100

    syncxmlsrv

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace syncxmlsrv
+{
+    public class Message
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Msg { get; set; }
+        public string Stamp { get; set; }
+
+        public override string ToString()
+        {
+            return $"From: {From}\nTo: {To}\n{Msg}\nStamp: {Stamp}";
+        }
+    }
+
+    public class SynchronousSocketListener
+    {
+        public static int TAM = 1024;
+        public static int PORT = 11000;
+
+        public static void StartListening()
+        {
+            byte[] bytes = new Byte[TAM];
+
+            IPAddress ipAddress = GetLocalIpAddress();
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, PORT);
+
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection at {0}...", ipAddress);
+                    Socket handler = listener.Accept();
+
+                    Message request = Receive(handler);
+                    Console.WriteLine(request);
+
+                    Message response = new Message { From = "0", To = request.From, Msg = "Adios!", Stamp = "Server" };
+                    Send(handler, response);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+
+        private static IPAddress GetLocalIpAddress()
+        {
+            List<IPAddress> ipAddressList = new List<IPAddress>();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            int t = ipHostInfo.AddressList.Length;
+            string ip;
+            for (int i = 0; i < t; i++)
+            {
+                ip = ipHostInfo.AddressList[i].ToString();
+                if (ip.Contains(".") && !ip.Equals("127.0.0.1")) ipAddressList.Add(ipHostInfo.AddressList[i]);
+            }
+            if (ipAddressList.Count == 1)
+            {
+                return ipAddressList[0];
+            }
+            else
+            {
+                int i = 0;
+                foreach (IPAddress ipa in ipAddressList)
+                {
+                    Console.WriteLine($"[{i++}]: {ipa}");
+                }
+                t = ipAddressList.Count - 1;
+                System.Console.Write($"Opción [0-{t}]: ");
+                string s = Console.ReadLine();
+                if (Int32.TryParse(s, out int j))
+                {
+                    if ((j >= 0) && (j <= t))
+                    {
+                        return ipAddressList[j];
+                    }
+                }
+                return null;
+            }
+        }
+
+        private static void Send(Socket socket, Message message)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Message));
+            Stream stream = new MemoryStream();
+            serializer.Serialize(stream, message);
+            byte[] byteData = ((MemoryStream)stream).ToArray();
+            // string xml = Encoding.ASCII.GetString(byteData, 0, byteData.Length);
+            // Console.WriteLine(xml);
+            int bytesSent = socket.Send(byteData);
+        }
+
+        private static Message Receive(Socket socket)
+        {
+            byte[] bytes = new byte[TAM];
+            int bytesRec = socket.Receive(bytes);
+            string xml = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            // Console.WriteLine(xml);
+            byte[] byteArray = Encoding.ASCII.GetBytes(xml);
+            MemoryStream stream = new MemoryStream(byteArray);
+            Message response = (Message)new XmlSerializer(typeof(Message)).Deserialize(stream);
+            return response;
+        }
+
+        public static int Main(String[] args)
+        {
+            StartListening();
+            return 0;
+        }
+    }
+}
